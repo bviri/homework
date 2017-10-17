1bdbf9a92381a8ecbb505a60dd641d3bdb14fea1
bea <31502382+bviri@users.noreply.github.com>
2017-10-17 17:14:25 -0400

    Create baking.ino

---
+++ baking.ino
@@ -0,0 +1,33 @@
+int preHeat = 8;
+int ready = 9;
+int tooHot = 10;
+
+void setup() {
+  pinMode(preHeat, OUTPUT); 
+  pinMode(9, OUTPUT); 
+  pinMode(10, OUTPUT); 
+}
+
+void loop() {
+  int bakingTemp = analogRead(A0);
+
+  if (bakingTemp < 275){
+    digitalWrite(preHeat, HIGH);
+    delay(1000);
+    digitalWrite(preHeat, LOW); 
+    delay(1000); 
+    
+  } else if (bakingTemp > 276 && bakingTemp < 400){
+    digitalWrite(ready, HIGH);
+    delay(500);
+    digitalWrite(ready, LOW);
+    delay(500);
+    
+  } else if (bakingTemp > 401 && bakingTemp < 900) {
+    digitalWrite(tooHot, HIGH);
+    delay(100);
+    digitalWrite(tooHot,LOW);
+    delay(100);
+  } 
+ 
+}
